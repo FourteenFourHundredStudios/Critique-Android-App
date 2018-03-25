9ad25e44e538cfd19f069c815f490db3fc38e71b
FourteenFourHundredStudios <marcfervil630@gmail.com>
2018-03-25 16:08:28 -0500

    Que Changes
    Que now uses ViewFlipper...

--- app/src/main/java/critique/fourteenfourhundred/marc/critique/QueHolderFragment.java
+++ app/src/main/java/critique/fourteenfourhundred/marc/critique/QueHolderFragment.java
@@ -7,10 +7,18 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.NestedScrollView;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.ViewFlipper;
+import android.widget.ViewSwitcher;
 
 import org.json.JSONObject;
 
@@ -20,8 +28,10 @@ import java.util.ArrayList;
 public class QueHolderFragment extends Fragment implements View.OnClickListener {
 
     public View rootView;
-    public DeactivatedViewPager viewPager;
+    public ViewFlipper layout;
     public ArrayList<QueFragment> posts=new ArrayList<QueFragment>();
+    public Animation slide_in_left, slide_out_right;
+
 
     public QueHolderFragment() {
         // Required empty public constructor
@@ -32,10 +42,9 @@ public class QueHolderFragment extends Fragment implements View.OnClickListener 
         try {
             switch (view.getId()) {
                 case R.id.voteGood:
-                    viewPager.arrowScroll(View.FOCUS_RIGHT);
 
-                    //Util.showDialog(getActivity(),"GOOD!");
-                    //Data.lastVote=1;
+
+                    layout.showNext();
 
                     /*
                     API.castVotes(getActivity(), currentPost.getString("_id"),1,new Callback(){
@@ -67,25 +76,32 @@ public class QueHolderFragment extends Fragment implements View.OnClickListener 
         rootView = inflater.inflate(R.layout.fragment_que_holder, container, false);
 
 
-        viewPager = (DeactivatedViewPager) rootView.findViewById(R.id.pageHolder);
+        //content = (LockableScrollView) rootView.findViewById(R.id.pageHolder);
+
+        layout = (ViewFlipper) rootView.findViewById(R.id.contentHolder);
+
+        slide_in_left = AnimationUtils.loadAnimation(getActivity(),
+                android.R.anim.slide_in_left);
+        slide_out_right = AnimationUtils.loadAnimation(getActivity(),
+                android.R.anim.slide_out_right);
+
+        layout.setInAnimation(slide_in_left);
+        layout.setOutAnimation(slide_out_right);
+
+
+        //content = (LockableScrollView) rootView.findViewById(R.id.pageHolder);
+        //content.setScrollingEnabled(true);
+        //content
+
+
+        //content.setOnTouchListener(null);
+
+
+        //getFragmentManager().beginTransaction().add(layout.getId(), QueFragment.instantiate(getContext(),"que"), "someTag1").commit();
 
 
 
 
-
-        // Create an adapter that knows which fragment should be shown on each page
-        QueHolderAdapter adapter = new QueHolderAdapter(getContext(), getFragmentManager());
-        viewPager.setAdapter(adapter);
-
-
-        viewPager.setFocusableInTouchMode(false);
-        viewPager.setFocusable(false);
-        viewPager.setEnabled(false);
-        viewPager.setClickable(false);
-        viewPager.setFocusableInTouchMode(false);
-
-
-        viewPager.setPagingEnabled(false);
 
         ImageButton voteBad = (ImageButton) rootView.findViewById(R.id.voteBad);
         voteBad.setOnClickListener(this);
@@ -93,53 +109,27 @@ public class QueHolderFragment extends Fragment implements View.OnClickListener 
         ImageButton voteGood = (ImageButton) rootView.findViewById(R.id.voteGood);
         voteGood.setOnClickListener(this);
 
+
+
+//        ((ScrollView)rootView.findViewById(R.id.pageHolder)).requestDisallowInterceptTouchEvent(true);
+
+
         for(int i=0;i<6;i++){
             QueFragment post = new QueFragment();
             Bundle bundle=new Bundle();
-            bundle.putString("post","{\"username\":\"marc\",\"title\":\"Nooo\",\"type\":\"text\",\"seen\":[],\"votes\":0,\"to\":[\"john\"],\"content\":\"NOPE!\"}");
+            bundle.putString("post","{\"username\":\"marc\",\"title\":\"Nooo"+i+"\",\"type\":\"text\",\"seen\":[],\"votes\":0,\"to\":[\"john\"],\"content\":\"orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.!\"}");
             post.setArguments(bundle);
             posts.add(post);
+            getFragmentManager().beginTransaction().add(layout.getId(), post, "someTag1").commit();
         }
+
+
 
         return rootView;
     }
 
-    class QueHolderAdapter extends FragmentStatePagerAdapter {
-        private Context mContext;
 
 
-
-        public QueHolderAdapter(Context context, FragmentManager fm) {
-            super(fm);
-            mContext = context;
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            //  ProfileFragment tab1 = new ProfileFragment();
-            return posts.get(position);
-
-        }
-
-        @Override
-        public int getCount() {
-
-            return 5;
-        }
-
-        public CharSequence getPageTitle(int position) {
-            // Generate title based on item position
-            switch (position) {
-                case 0:
-                    return "Que";
-                default:
-                    return null;
-            }
-        }
-
-
-
-    }
 
 }
 
