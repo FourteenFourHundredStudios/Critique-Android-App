66b4236ce34b04597827a2ec7a3c6e41ffb67ba7
FourteenFourHundredStudios <marcfervil630@gmail.com>
2018-03-25 18:23:12 -0500

    Que Works
    Kinda

--- app/src/main/java/critique/fourteenfourhundred/marc/critique/QueHolderFragment.java
+++ app/src/main/java/critique/fourteenfourhundred/marc/critique/QueHolderFragment.java
@@ -20,6 +20,7 @@ import android.widget.ScrollView;
 import android.widget.ViewFlipper;
 import android.widget.ViewSwitcher;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
@@ -29,9 +30,15 @@ public class QueHolderFragment extends Fragment implements View.OnClickListener 
 
     public View rootView;
     public ViewFlipper layout;
-    public ArrayList<QueFragment> posts=new ArrayList<QueFragment>();
+    //public ArrayList<QueFragment> posts=new ArrayList<QueFragment>();
     public Animation slide_in_left, slide_out_right;
+    public JSONObject currentPost;
 
+    public int remainingPosts=2;
+    public int currentPostCount=0;
+
+
+    public static ArrayList<JSONObject> que = new ArrayList<JSONObject>();
 
     public QueHolderFragment() {
         // Required empty public constructor
@@ -40,30 +47,27 @@ public class QueHolderFragment extends Fragment implements View.OnClickListener 
     @Override
     public void onClick(View view) {
         try {
+            currentPost=que.get(currentPostCount);
             switch (view.getId()) {
                 case R.id.voteGood:
 
-
-                    layout.showNext();
-
-                    /*
                     API.castVotes(getActivity(), currentPost.getString("_id"),1,new Callback(){
                         @Override
                         public void onResponse(JSONObject response) {
                             loadPost();
                         }
-                    });*/
+                    });
 
                     return;
                 case R.id.voteBad:
                     //Data.lastVote=0;
-                    /*
+
                     API.castVotes(getActivity(), currentPost.getString("_id"),0,new Callback(){
                         @Override
                         public void onResponse(JSONObject response) {
                             loadPost();
                         }
-                    });*/
+                    });
                     return;
             }
         } catch (Exception e) {
@@ -71,36 +75,19 @@ public class QueHolderFragment extends Fragment implements View.OnClickListener 
         }
     }
 
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.fragment_que_holder, container, false);
 
-
-        //content = (LockableScrollView) rootView.findViewById(R.id.pageHolder);
-
         layout = (ViewFlipper) rootView.findViewById(R.id.contentHolder);
 
-        slide_in_left = AnimationUtils.loadAnimation(getActivity(),
-                android.R.anim.slide_in_left);
-        slide_out_right = AnimationUtils.loadAnimation(getActivity(),
-                android.R.anim.slide_out_right);
+        slide_in_left = AnimationUtils.loadAnimation(getActivity(),android.R.anim.slide_in_left);
+        slide_out_right = AnimationUtils.loadAnimation(getActivity(),android.R.anim.slide_out_right);
 
         layout.setInAnimation(slide_in_left);
         layout.setOutAnimation(slide_out_right);
-
-
-        //content = (LockableScrollView) rootView.findViewById(R.id.pageHolder);
-        //content.setScrollingEnabled(true);
-        //content
-
-
-        //content.setOnTouchListener(null);
-
-
-        //getFragmentManager().beginTransaction().add(layout.getId(), QueFragment.instantiate(getContext(),"que"), "someTag1").commit();
-
-
-
 
 
         ImageButton voteBad = (ImageButton) rootView.findViewById(R.id.voteBad);
@@ -109,25 +96,58 @@ public class QueHolderFragment extends Fragment implements View.OnClickListener 
         ImageButton voteGood = (ImageButton) rootView.findViewById(R.id.voteGood);
         voteGood.setOnClickListener(this);
 
-
-
-//        ((ScrollView)rootView.findViewById(R.id.pageHolder)).requestDisallowInterceptTouchEvent(true);
-
-
-        for(int i=0;i<6;i++){
-            QueFragment post = new QueFragment();
-            Bundle bundle=new Bundle();
-            bundle.putString("post","{\"username\":\"marc\",\"title\":\"Nooo"+i+"\",\"type\":\"text\",\"seen\":[],\"votes\":0,\"to\":[\"john\"],\"content\":\"orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.!\"}");
-            post.setArguments(bundle);
-            posts.add(post);
-            getFragmentManager().beginTransaction().add(layout.getId(), post, "someTag1").commit();
-        }
-
+        loadPost();
 
 
         return rootView;
     }
 
+
+    public void loadPost(){
+
+
+            remainingPosts--;
+
+            if(remainingPosts==1) {
+
+                API.getQue(getActivity(), new Callback() {
+                    public void onResponse(JSONObject json) {
+                        que.clear();
+                        try {
+                            if (json.getString("status").equals("ok")) {
+
+                                JSONArray posts = new JSONArray(json.getString("message"));
+
+                                remainingPosts = posts.length();
+                                currentPostCount=0;
+
+                                //for(View f:)
+
+                                for (int i = 0; i < posts.length(); i++) {
+                                    QueFragment post = new QueFragment();
+                                    Bundle bundle = new Bundle();
+                                    bundle.putString("post", posts.get(i).toString());
+                                    post.setArguments(bundle);
+                                    que.add(new JSONObject(posts.get(i).toString()));
+                                    getFragmentManager().beginTransaction().add(layout.getId(), post).commit();
+                                }
+                                layout.showNext();
+                            }else{
+                                Util.showDialog(getActivity(),json.getString("message"));
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            }else {
+                currentPostCount++;
+                layout.showNext();
+            }
+
+
+
+    }
 
 
 
